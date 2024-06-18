9bbf17b302cc7be2be3d6525956cc0e4d1060785
Fabio Junior <fnjunior95@gmail.com>
2024-06-18 14:51:08 -0300

    listas

---
+++ src/ListaTarefas.java
@@ -0,0 +1,12 @@
+/* . Lista de Tarefas
+Crie uma classe chamada "ListaTarefas" que possui uma lista de tarefas como atributo. Cada tarefa é representada por uma classe chamada "Tarefa" que possui um atributo de descrição. Implemente os seguintes métodos:
+
+adicionarTarefa(String descricao): Adiciona uma nova tarefa à lista com a descrição fornecida.
+removerTarefa(String descricao): Remove uma tarefa da lista com base em sua descrição.
+obterNumeroTotalTarefas(): Retorna o número total de tarefas na lista.
+obterDescricoesTarefas(): Retorna uma lista contendo a descrição de todas as tarefas na lista.*/
+import java.util.ArrayList;
+import java.util.List;
+public class ListaTarefas{
+    
+}
---
+++ src/Tarefa.java
@@ -0,0 +1,16 @@
+/* . Lista de Tarefas
+Crie uma classe chamada "ListaTarefas" que possui uma lista de tarefas como atributo. Cada tarefa é representada por uma classe chamada "Tarefa" que possui um atributo de descrição. Implemente os seguintes métodos:
+
+adicionarTarefa(String descricao): Adiciona uma nova tarefa à lista com a descrição fornecida.
+removerTarefa(String descricao): Remove uma tarefa da lista com base em sua descrição.
+obterNumeroTotalTarefas(): Retorna o número total de tarefas na lista.
+obterDescricoesTarefas(): Retorna uma lista contendo a descrição de todas as tarefas na lista.*/    
+public class Tarefa{
+    private String descricao;
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+    public String getDescricao(){
+        return descricao;
+    }
+}
